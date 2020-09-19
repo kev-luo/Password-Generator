00ce8a99c8452ce26dd9c6f064420c04f9c570b1
Kevin Luo <kvn.luo@gmail.com>
2020-09-19 11:29:33 -0400

    add password selection criteria based on user input

--- script.js
+++ script.js
@@ -11,31 +11,40 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
     document.getElementById("password").textContent = generatePassword();
-
-    // var passwordText = document.querySelector("#password");
-    // generatePassword();
-    // passwordText.value = password;
-    // pLength();
-    // lCase();
-    // uCase();
-    // numeric();
-    // sChar();
 };
 
 // function to generate random string
 function generatePassword() {
     var finalString = '';
+    var charList = '';
     var lengthy = pLength();
     var littleL = lCase();
     var bigL = uCase();
     var numero = numeric();
     var especial = sChar();
-    var charList = lowerLetters+upperLetters+numbersList+specialCharacters;
-    for (var i=0; i<passLength; i++) {
-        finalString += charList.charAt(Math.floor(Math.random() * charList.length));
+    
+    // add to the charList string based on user responses
+    if (littleL) {
+        charList += lowerLetters;
     };
-    return finalString;
-}
+    if (bigL) {
+        charList += upperLetters;
+    };
+    if (numero) {
+        charList += numbersList;
+    };
+    if (especial) {
+        charList += specialCharacters;
+    };
+    if (!littleL && !bigL && !numero && !especial) {
+        alert("Your password must contain at least 1 of the following: lowercase letter, uppercase letters, numbers, or special characters");
+    } else {
+        for (var i=0; i<lengthy; i++) {
+            finalString += charList.charAt(Math.floor(Math.random() * charList.length));
+        };
+        return finalString;
+    };
+};
 
 // function for password length prompt
 function pLength() {
@@ -45,45 +54,32 @@ function pLength() {
     }
     console.log(numChar);
     return numChar;
-}
+};
 
 // function for lowercase letters
 function lCase() {
     var lowCase = confirm("Should your password include lowercase letters?");
     console.log(lowCase);
     return lowCase;
-}
+};
 
 // function for uppercase letters
 function uCase() {
     var upCase = confirm("Should your password include uppercase letters?");
     console.log(upCase);
     return upCase;
-}
+};
 
 // function for numbers
 function numeric() {
     var numb = confirm("Should your password include numbers?");
     console.log(numb);
     return numb;
-}
+};
 
 // function for special characters
 function sChar() {
     var speChar = confirm("Should your password include special characters?");
     console.log(speChar);
     return speChar;
-}
-
-
-
-
-
-// add event listener when button is clicked. function should present series of prompts for password criteria
-// prompt for lowercase, uppercase, numeric, and/or special characters - must select at least one
-// generate password when all prompts are answered
-// generated password should match all criteria
-// display generated password in an alert or written to the page
-
-// document.getElementById("password").textContent = "Hello";
-
+};
